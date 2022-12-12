33a76178fcbe089eafd2830041a38016963d10fb
syedmuhammaddanish <90153165+syedmuhammaddanish@users.noreply.github.com>
2022-12-12 14:49:47 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,23 @@ module.exports = {
          accounts: [`0x${PRIVATE_KEY}`],
          gas: 210000000,
          gasPrice: 800000000000,
+      },
+      matic: {
+         allowUnlimitedContractSize: true,
+        url: "https://rpc-mumbai.maticvigil.com",
+        accounts: [`0x${PRIVATE_KEY}`],
+        gas: 2100000,
+         gasPrice: 8000000000
+      },
+      iotex: {
+         url: `https://babel-api.testnet.iotex.io`,
+         accounts: [`0x${PRIVATE_KEY}`]
+      },
+      avax: {
+         url: 'https://api.avax-test.network/ext/bc/C/rpc',
+         gasPrice: 225000000000,
+         chainId: 43113,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
    },
 }
