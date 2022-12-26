8b3abdf9a8c843d54ae6a45806fc56d3410c6836
syedmuhammaddanish <90153165+syedmuhammaddanish@users.noreply.github.com>
2022-12-26 18:54:46 -0500

    Update FinancialCom.sol

--- contracts/FinancialCom.sol
+++ contracts/FinancialCom.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
 
 contract FinancialCom{
     int sum;
     mapping(string => mapping (address => int)) bill;
-//Maps date and customer information to the related energy consumption values
+    mapping(string => mapping (address => int)) data;
+
+    //Store billing data
+    function storeBill(int[] memory value, address[] memory customer, string memory date) public {
+        for(uint i = 0; i < value.length; i++){
+            data[date][customer[i]] = value[i];
+        }
+    }
+
+    //Maps date and customer information to the related energy consumption values
     function FinancialCalculation (int[] memory initialcon, int[] memory finalcon, int[] memory realcon, int initialprice, int finalprice, string memory date, address customer) public {
     sum = 0;
     for(uint i = 0; i < 144; i++){
@@ -19,8 +28,24 @@ contract FinancialCom{
         }
      }
      bill[date][customer] = sum;
-}
-    function TotalPrice (string memory date, address customer) public view returns(int) {
+    }   
+
+    //Return total price
+    function CalculatedBalance (string memory date, address customer) public view returns(int) {
+        return data[date][customer];
+    }
+
+    function VerifiedBalance (string memory date, address customer) public view returns(int) {
         return bill[date][customer];
+    }
+
+    //Validate the financial calculation
+    function TotalPriceCheck (int value, string memory date, address customer) public view returns(bool) {
+        if (value == bill[date][customer]) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
